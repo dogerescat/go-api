b5344c932e680e6281db3f8e59e2b72c2fc4a579
大庭秀斗 <shuto.0028.0618@gmail.com>
2022-09-02 23:33:51 +0900

    try gin sample

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "welcome to my website")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
 	})
-	http.ListenAndServe(":80", nil)
+	r.Run(":80")
 }
