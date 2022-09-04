0b5342c84c3300f15c12f3800b9a76e4f7f4a958
大庭秀斗 <shuto.0028.0618@gmail.com>
2022-09-05 00:21:24 +0900

    try example router

--- backend/src/main.go
+++ backend/src/main.go
@@ -8,10 +8,18 @@ import (
 
 func main() {
 	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+	users := r.Group("/users")
+	{
+		users.GET("/hello", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "hello world",
+			})
 		})
-	})
+		users.POST("/login", func(c *gin.Context) {
+			c.JSON(http.StatusOK, gin.H{
+				"message": "login",
+			})
+		})
+	}
 	r.Run(":80")
 }
